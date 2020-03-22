62b9e058b14d6b4689306cea516fb3de3c87ac22
Heisenberg <Persey_98@list.ru>
2020-03-22 11:34:21 +0500

    Add calculator

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Give me a number:");
+            int number1 = Convert.ToInt32 (Console.ReadLine ());
+            Console.WriteLine ("Give me another number:");
+            int number2 = Convert.ToInt32 (Console.ReadLine ());
+            var sum = number1 + number2;
+            var divn = number1 / number2;
+            var mult = number1 * number2;
+            var subt = number1 - number2;
+            Console.WriteLine ("sum is = {0}; divn is = {1}; mult is = {2}; subt is = {3}", sum,divn,mult,subt);
+        }
+    }
+}
